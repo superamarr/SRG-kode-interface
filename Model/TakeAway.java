3fb82698a4004d9c7a12f281f0017bfc33c33a10
Taufik Ramadhani <dhaniiramaaa533@gmail.com>
2025-09-30 19:10:42 +0800

    Add files via upload

---
+++ Model/TakeAway.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Aspire5
+ */
+public class TakeAway extends Reservasi {
+    private boolean sudahAmbil;
+    
+    public TakeAway(String nama, String waktu, String preOrder, String namaMenu, int harga, boolean sudahDiambil) {
+        super(nama, waktu, "-", preOrder, namaMenu, harga);
+        this.sudahAmbil = sudahAmbil;
+    }
+    
+    public boolean issudahAmbil(){
+        return sudahAmbil;
+    }
+    
+    public void setsudahAmbil (boolean sudahAmbil){
+        this.sudahAmbil = sudahAmbil;
+    }
+    
+    @Override
+    public String getDetail() {
+        return "Reservasi Take-Away, status: " + (sudahAmbil ? "Sudah diambil" : "Belum diambil");
+    }
+}
